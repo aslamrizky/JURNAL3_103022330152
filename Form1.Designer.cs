e24d79369358a194dd8b25ba73030c2b09f837a4
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-06 10:09:18 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace MODUL3_103022330152
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tambah = new System.Windows.Forms.Button();
+            this.samadengan = new System.Windows.Forms.Button();
+            this.nol = new System.Windows.Forms.Button();
+            this.satu = new System.Windows.Forms.Button();
+            this.dua = new System.Windows.Forms.Button();
+            this.tiga = new System.Windows.Forms.Button();
+            this.empat = new System.Windows.Forms.Button();
+            this.lima = new System.Windows.Forms.Button();
+            this.enam = new System.Windows.Forms.Button();
+            this.tujuh = new System.Windows.Forms.Button();
+            this.delapan = new System.Windows.Forms.Button();
+            this.sembilan = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tambah
+            // 
+            this.tambah.Location = new System.Drawing.Point(155, 105);
+            this.tambah.Name = "tambah";
+            this.tambah.Size = new System.Drawing.Size(51, 44);
+            this.tambah.TabIndex = 0;
+            this.tambah.Text = "+";
+            this.tambah.UseVisualStyleBackColor = true;
+            this.tambah.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // samadengan
+            // 
+            this.samadengan.Location = new System.Drawing.Point(212, 105);
+            this.samadengan.Name = "samadengan";
+            this.samadengan.Size = new System.Drawing.Size(51, 44);
+            this.samadengan.TabIndex = 1;
+            this.samadengan.Text = "=";
+            this.samadengan.UseVisualStyleBackColor = true;
+            // 
+            // nol
+            // 
+            this.nol.Location = new System.Drawing.Point(269, 105);
+            this.nol.Name = "nol";
+            this.nol.Size = new System.Drawing.Size(51, 44);
+            this.nol.TabIndex = 2;
+            this.nol.Text = "0";
+            this.nol.UseVisualStyleBackColor = true;
+            // 
+            // satu
+            // 
+            this.satu.Location = new System.Drawing.Point(155, 155);
+            this.satu.Name = "satu";
+            this.satu.Size = new System.Drawing.Size(51, 44);
+            this.satu.TabIndex = 3;
+            this.satu.Text = "1";
+            this.satu.UseVisualStyleBackColor = true;
+            // 
+            // dua
+            // 
+            this.dua.Location = new System.Drawing.Point(212, 155);
+            this.dua.Name = "dua";
+            this.dua.Size = new System.Drawing.Size(51, 44);
+            this.dua.TabIndex = 4;
+            this.dua.Text = "2";
+            this.dua.UseVisualStyleBackColor = true;
+            // 
+            // tiga
+            // 
+            this.tiga.Location = new System.Drawing.Point(269, 155);
+            this.tiga.Name = "tiga";
+            this.tiga.Size = new System.Drawing.Size(51, 44);
+            this.tiga.TabIndex = 5;
+            this.tiga.Text = "3";
+            this.tiga.UseVisualStyleBackColor = true;
+            // 
+            // empat
+            // 
+            this.empat.Location = new System.Drawing.Point(155, 205);
+            this.empat.Name = "empat";
+            this.empat.Size = new System.Drawing.Size(51, 44);
+            this.empat.TabIndex = 6;
+            this.empat.Text = "4";
+            this.empat.UseVisualStyleBackColor = true;
+            // 
+            // lima
+            // 
+            this.lima.Location = new System.Drawing.Point(212, 205);
+            this.lima.Name = "lima";
+            this.lima.Size = new System.Drawing.Size(51, 44);
+            this.lima.TabIndex = 7;
+            this.lima.Text = "5";
+            this.lima.UseVisualStyleBackColor = true;
+            // 
+            // enam
+            // 
+            this.enam.Location = new System.Drawing.Point(269, 205);
+            this.enam.Name = "enam";
+            this.enam.Size = new System.Drawing.Size(51, 44);
+            this.enam.TabIndex = 8;
+            this.enam.Text = "6";
+            this.enam.UseVisualStyleBackColor = true;
+            // 
+            // tujuh
+            // 
+            this.tujuh.Location = new System.Drawing.Point(155, 255);
+            this.tujuh.Name = "tujuh";
+            this.tujuh.Size = new System.Drawing.Size(51, 44);
+            this.tujuh.TabIndex = 9;
+            this.tujuh.Text = "7";
+            this.tujuh.UseVisualStyleBackColor = true;
+            // 
+            // delapan
+            // 
+            this.delapan.Location = new System.Drawing.Point(212, 255);
+            this.delapan.Name = "delapan";
+            this.delapan.Size = new System.Drawing.Size(51, 44);
+            this.delapan.TabIndex = 10;
+            this.delapan.Text = "8";
+            this.delapan.UseVisualStyleBackColor = true;
+            // 
+            // sembilan
+            // 
+            this.sembilan.Location = new System.Drawing.Point(269, 255);
+            this.sembilan.Name = "sembilan";
+            this.sembilan.Size = new System.Drawing.Size(51, 44);
+            this.sembilan.TabIndex = 11;
+            this.sembilan.Text = "9";
+            this.sembilan.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sembilan);
+            this.Controls.Add(this.delapan);
+            this.Controls.Add(this.tujuh);
+            this.Controls.Add(this.enam);
+            this.Controls.Add(this.lima);
+            this.Controls.Add(this.empat);
+            this.Controls.Add(this.tiga);
+            this.Controls.Add(this.dua);
+            this.Controls.Add(this.satu);
+            this.Controls.Add(this.nol);
+            this.Controls.Add(this.samadengan);
+            this.Controls.Add(this.tambah);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tambah;
+        private System.Windows.Forms.Button samadengan;
+        private System.Windows.Forms.Button nol;
+        private System.Windows.Forms.Button satu;
+        private System.Windows.Forms.Button dua;
+        private System.Windows.Forms.Button tiga;
+        private System.Windows.Forms.Button empat;
+        private System.Windows.Forms.Button lima;
+        private System.Windows.Forms.Button enam;
+        private System.Windows.Forms.Button tujuh;
+        private System.Windows.Forms.Button delapan;
+        private System.Windows.Forms.Button sembilan;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
